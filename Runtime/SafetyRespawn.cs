a7bd9d2eb3c1dc083072cc7f293465f9079a9a3d
Silent <16026653+s-ilent@users.noreply.github.com>
2024-09-30 18:24:49 +0930

    Add code and etc.

---
+++ Runtime/SafetyRespawn.cs
@@ -0,0 +1,55 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using UnityEngine.AI;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class SafetyRespawn : UdonSharpBehaviour
+{
+    [Header("Tracks the player's last good position. Call ResetPlayerPosition to respawn them there.")]
+    public LayerMask groundLayer;
+    public float maxAngle = 45f;
+    private Vector3 lastSafePosition;
+    private Quaternion lastSafeRotation;
+    // VRC stuff
+    private VRCPlayerApi localPlayer;
+    private bool localPlayerCached = false;
+
+    public void Start()
+    {
+        localPlayer = Networking.LocalPlayer;
+        if (localPlayer != null)
+        {
+            localPlayerCached = true;
+        }
+    }
+
+    // Deliberately does not replace the base respawn function. 
+
+    void Update()
+    {
+        if (!localPlayerCached) return;
+        if (!Utilities.IsValid(localPlayer)) return;
+
+        Vector3 localPlayerPosition = localPlayer.GetPosition();
+        Quaternion localPlayerRotation = localPlayer.GetRotation();
+        
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(localPlayerPosition, out hit, 1.0f, NavMesh.AllAreas))
+        {
+            float angle = Vector3.Angle(hit.normal, Vector3.up);
+            if (angle <= maxAngle)
+            {
+                lastSafePosition = localPlayerPosition;
+                lastSafeRotation = localPlayerRotation;
+            }
+        }
+    }
+
+    public void ResetPlayerPosition()
+    {
+		localPlayer.TeleportTo(lastSafePosition + new Vector3(0f, 0.01f, 0f), lastSafeRotation);
+    }
+}
---
+++ Runtime/SafetyRespawner.cs
@@ -0,0 +1,47 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class SafetyRespawner : UdonSharpBehaviour
+{
+    [Header("Respawns the player on contact.")]
+    [Tooltip("Set the main Safety Respawner object here.")]
+    public SafetyRespawn safetyRespawn;
+    
+    // VRC stuff
+    private VRCPlayerApi localPlayer;
+    private bool localPlayerCached = false;
+
+    public void Start()
+    {
+        localPlayer = Networking.LocalPlayer;
+        if (localPlayer != null)
+        {
+            localPlayerCached = true;
+        }
+        // If no coordinator script, die
+        if (safetyRespawn == null) this.enabled = false;
+    }
+
+    // Used if the collider is a trigger.
+    public override void OnPlayerTriggerEnter(VRC.SDKBase.VRCPlayerApi player)
+    {
+        applyRespawn(player);
+    }
+    // Used if the collider is not a trigger.
+    // As far as I can tell, players don't really "enter" solid colliders. 
+    public override void OnPlayerCollisionEnter(VRC.SDKBase.VRCPlayerApi player)
+    {
+        applyRespawn(player);
+    }
+    // No reason to handle OnCollisionEnter for non-players yet
+    private void applyRespawn(VRC.SDKBase.VRCPlayerApi player)
+    {
+        if (!(localPlayerCached && Utilities.IsValid(localPlayer))) return;
+        if (player != localPlayer) return;
+        safetyRespawn.ResetPlayerPosition();
+    }
+}
